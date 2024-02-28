8154a31150fa755aa13fcb584da5afb114bafdc9
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-02-29 04:52:40 +0800

    Add MultiResUNet option

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ from tqdm import tqdm
 import wandb
 from evaluate import evaluate
 from unet import UNet
+from multiresunet import MultiResUNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
@@ -169,6 +170,7 @@ def train_model(
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
+    parser.add_argument('--arch', type=str, choices=['unet', 'multiresunet'], default='unet')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
@@ -194,7 +196,9 @@ if __name__ == '__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear) \
+        if args.arch == "unet" else \
+        MultiResUNet(n_channels=3, n_classes=args.classes)
     model = model.to(memory_format=torch.channels_last)
 
     logging.info(f'Network:\n'
